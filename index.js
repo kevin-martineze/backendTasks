3a0c1e43919622d4cd4a355ed559c1d8089dd06f
Kevin Martínez <54540601+kevin-martineze@users.noreply.github.com>
2023-11-25 22:37:49 -0500

    Error de cors

--- index.js
+++ index.js
@@ -13,22 +13,7 @@ dotenv.config();
 
 conectarDB();
 
-// Configurar CORS
-const whitelist = [process.env.FRONTEND_URL];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      // Puede consultar la API
-      callback(null, true);
-    } else {
-      // No esta permitido
-      callback(new Error("Error de Cors"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Routing
 app.use("/api/usuarios", usuarioRoutes);
@@ -46,7 +31,7 @@ import { Server } from "socket.io";
 const io = new Server(servidor, {
   pingTimeout: 60000,
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
   },
 });
 
